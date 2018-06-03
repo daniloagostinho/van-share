e1c0e95fdb7b60aac7729d622d28be9fbfd5797c
daniloagostinhosilva@gmail.com <danilosilva@MacBook-Air-de-Danilo.local>
2018-06-03 10:15:23 -0300

    criando as telas

--- src/components/confirmacao/confirmacao.ts
+++ src/components/confirmacao/confirmacao.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { NavController } from 'ionic-angular';
+import { NotificoesComponent } from '../notificoes/notificoes';
+
+
 @Component({
   selector: 'confirmacao',
   templateUrl: 'confirmacao.html'
@@ -8,7 +12,11 @@ export class ConfirmacaoComponent {
 
   text: string;
 
-  constructor() {
+  constructor(public navCtrl: NavController) {
   }
+
+  goToNotificacoes(){
+    this.navCtrl.push(NotificoesComponent);
+   }
 
 }
